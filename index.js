9e44d833b1ff7a4bd38ec8a6e03af409d4382e36
Justin Palmer <encytemedia@gmail.com>
2021-11-04 10:36:43 -0700

    start me up

---
+++ index.js
@@ -0,0 +1,19 @@
+import {
+  ElementaryWebAudioRenderer as core,
+  el,
+} from "@nick-thompson/elementary";
+
+const ctx = new (window.AudioContext || window.webkitAudioContext)();
+
+core.on("load", function () {
+  core.render(el.cycle(440), el.cycle(441));
+});
+
+(async function main() {
+  let node = await core.initialize(ctx, {
+    numberOfInputs: 0,
+    numberOfOutputs: 1,
+    outputChannelCount: [2],
+  });
+  node.connect(ctx.destination);
+})();
